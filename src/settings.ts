699f90305fbf610d759accef420f6d9e48250f1a
MS-CEHGZMWKKNDH\Administrator <760902694@qq.com>
2024-01-02 23:47:01 +0800

    feat:'项目初始化'

---
+++ src/settings.ts
@@ -0,0 +1,16 @@
+const defaultSettings: AppSettings = {
+  title: "小程序管理系统",
+  version: "v2.8.0",
+  showSettings: true,
+  tagsView: true,
+  fixedHeader: false,
+  sidebarLogo: true,
+  layout: "left",
+  theme: "light",
+  size: "default",
+  language: "zh-cn",
+  themeColor: "#409EFF",
+  watermark: { enabled: false, content: "有来技术" },
+};
+
+export default defaultSettings;
